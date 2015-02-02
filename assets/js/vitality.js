240e85c113cef73bd94584bb753f14d6f270f2f1
Your Name <you@example.com>
2015-02-02 18:35:09 +0200

    post analytic to mongolab

--- assets/js/vitality.js
+++ assets/js/vitality.js
@@ -171,12 +171,13 @@ $(document).ready(function() {
 window.postAnalytic = function (behaviour)  {
     behaviour = behaviour || "";
     $.getJSON("http://ip-api.com/json/?callback",function (ipdata) {
-        console.log(ipdata);
+        var analyticdata = {url: window.location.href, ip: ipdata}
 
         $.ajax({
-            url: "//api.mongolab.com/api/1/databases/quangpham-com/collections/website?apiKey=JDJZ1z9STUdxPyqDqmjVqwsHahD9sAXd",
-            ipdata: JSON.stringify( { "x" : 1 } ),
+            url: "https://api.mongolab.com/api/1/databases/quangpham-com/collections/website?apiKey=JDJZ1z9STUdxPyqDqmjVqwsHahD9sAXd",
+            data: JSON.stringify(analyticdata),
             type: "POST",
+            crossDomain : true,
             contentType: "application/json" 
         });
     })
