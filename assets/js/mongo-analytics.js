4e2e4cff8b42cdce1aff9237f30c559bf47cb949
Your Name <you@example.com>
2015-02-03 09:37:04 +0200

    ok

--- assets/js/mongo-analytics.js
+++ assets/js/mongo-analytics.js
@@ -1,19 +1,33 @@
-window.postAnalytic = function (behaviour)  {
-    behaviour = behaviour || "";
+windows.sendDataToMongolab = function(dbcollection, data) {
+    var created_time = new Date();
+    data.browser = navigator.userAgent;
+    data.created_at = created_time.toString();
+    data.timestamp = created_time.getTime();
+    data.screen_size = $(window).width().toString() + "x" + $(window).height().toString()
+
+    $.ajax({
+        url: "https://api.mongolab.com/api/1/databases/quangpham-com/collections/" + dbcollection + "?apiKey=JDJZ1z9STUdxPyqDqmjVqwsHahD9sAXd",
+        data: JSON.stringify(data),
+        type: "POST",
+        crossDomain : true,
+        contentType: "application/json" 
+    });
+}
+
+window.postAnalytic = function() {
+    // behaviour = behaviour || "";
     $.getJSON("http://ip-api.com/json/?callback",function (ipdata) {
 
-        if (ipdata.query == "88.195.149.153") {
-            return;
-        }
-        var analyticdata = {url: window.location.href, ip: ipdata, browser: navigator.userAgent, created_at: Date.now()}
+        // if (ipdata.query == "88.195.149.153") {
+        //     return;
+        // }
 
-        $.ajax({
-            url: "https://api.mongolab.com/api/1/databases/quangpham-com/collections/website?apiKey=JDJZ1z9STUdxPyqDqmjVqwsHahD9sAXd",
-            data: JSON.stringify(analyticdata),
-            type: "POST",
-            crossDomain : true,
-            contentType: "application/json" 
-        });
+        var analyticdata = {
+            url: window.location.href, 
+            ip: ipdata
+        }
+
+        windows.sendDataToMongolab("website", analyticdata);
     })
 }
 
