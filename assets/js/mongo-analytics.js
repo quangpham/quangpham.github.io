c9b7603b6c6c6492d62a0b292fc6e413d1ca2757
Your Name <you@example.com>
2015-02-05 13:02:49 +0200

    add black list

--- assets/js/mongo-analytics.js
+++ assets/js/mongo-analytics.js
@@ -22,13 +22,16 @@ window.sendDataToMongolab = function(dbcollection, data) {
     if (data.ip.query == "88.195.149.153") { return; }
     if (data.ip.isp == "Tumblr") { return; }
 
-    var created_time = new Date();
-
     data.browser = navigator.userAgent;
-    data.created_at = created_time.toString();
-    data.timestamp = created_time.getTime();
+    if (data.browser == "gosquared-thumbnailer/1.0") { return; }
+
     data.screen_size = $(window).width().toString() + "x" + $(window).height().toString();
     data.qp_uuid = $.cookie('qpa_uuid');
+
+    var created_time = new Date();
+    data.created_at = created_time.toString();
+    data.timestamp = created_time.getTime();
+    
     
     $.ajax({
         url: "https://api.mongolab.com/api/1/databases/quangpham-com/collections/" + dbcollection + "?apiKey=JDJZ1z9STUdxPyqDqmjVqwsHahD9sAXd",
