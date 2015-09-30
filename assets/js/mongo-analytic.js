81935f72101adcd721169938f33f2aa7a8fd8ddc
Your Name <you@example.com>
2015-09-30 13:56:34 +0300

    ok

--- assets/js/mongo-analytic.js
+++ assets/js/mongo-analytic.js
@@ -18,6 +18,7 @@ window.sendDataToMongolab = function(dbcollection, data) {
     
     data.ip = JSON.parse(Cookies('qpa_ipdata'));
     if (data.ip.isp == "Tumblr") { return; }
+    if (data.ip.isp == "Googlebot") { return; }
 
     data.browser = navigator.userAgent;
     if (data.browser == "gosquared-thumbnailer/1.0") { return; }
